1512a51504adb37d68cadac23483b2ced1de5e2d
AspOfRodrigues <48492984+AspOfRodrigues@users.noreply.github.com>
2019-07-10 03:49:34 -0300

    Add mais 2 extras: praticas de POO em C#
    Add: pratica 1 de POO em C#
    Add: pratica 2 de POO em C3

---
+++ perso/C# extras/Pratica_1_POO/Pratica_1_POO/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica_1_POO
+{
+    public class Circle
+    {
+        private double x;
+        private double y;
+        private double r;
+
+        public Circle()
+        {
+            x = 0.0;
+            y = 0.0;
+            r = 0.0;
+            Console.WriteLine("new Circle");
+        }
+
+        public Circle(double x, double y, double r)
+        {
+            this.x = x;
+            this.y = y;
+            this.r = r;
+            Console.WriteLine("new Circle");
+        }
+
+        public double area()
+        {
+            return Math.PI * r * r;
+        }
+        public void setRadius(double radius)
+        {
+            this.r = radius;
+        }
+    }
+
+    public class TestCircle
+    {
+        static void main(String[] args)
+        {
+            Circle c1 = new Circle();
+            Circle c2 = new Circle();
+            Console.WriteLine("Area c1: " + c1.area());
+            Console.WriteLine("Area c2: " + c2.area());
+        }
+    }
+
+
+    public class Point
+    {
+        public double x;
+        public double y;
+
+        public Point()
+        {
+            this.x = 0.0;
+            this.y = 0.0;
+        }
+
+        public void desloca (double dx, double dy)
+        {
+            this.x = x + dx;
+            this.y = y + dy;
+        }
+
+        public static double distancia(double x1, double y1, double x2, double y2)
+        {
+            double dx = x2 - x1;
+            double dy = y2 - y1;
+            double distancia = Math.Sqrt(dx * dx + dy * dy);
+            return distancia;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle();
+            c.setRadius(5.0);
+            Console.WriteLine("Area do circulo = " + c.area());
+            Circle c2 = new Circle();
+            c2.setRadius(5.0);
+            Circle new_c1 = new Circle(1.0, 1.0, 4.0);
+            Circle new_c2 = new Circle(2.0, 2.0, 6.0);
+            Console.WriteLine("Area do circulo new_c1 =" + new_c1.area());
+            Console.WriteLine("Area do circulo new_c2 =" + new_c2.area());
+            Circle[] circs = new Circle[10];
+            for(int i = 0; i < circs.Length; i++)
+            {
+                circs[i] = new Circle();
+            }
+            Point p1 = new Point();
+            Point p2 = new Point();
+
+            p1.desloca(1.0, 1.0);
+            p2.desloca(2.0, 2.0);
+
+            double dist = Point.distancia(p1.x, p1.y, p2.x, p2.y);
+
+            Console.WriteLine("Distancia entre p1 e p2 = " + dist);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ perso/C# extras/Pratica_2_POO/Pratica_2_POO/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica_2_POO
+{
+    class Data
+    {
+        private int dia;
+        private int mes;
+        private int ano;
+
+        public Data (int dia, int mes, int ano)
+        {
+            this.dia = dia;
+            this.mes = mes;
+            this.ano = ano;
+        }
+
+        public void setData(int dia, int mes, int ano)
+        {
+            int[] diasmes = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (mes > 0 && mes < 13)
+            {
+                this.mes = mes;
+                // validando o dia de acordo com o mes
+                if (dia > 0 && dia <= diasmes[mes + 1])
+                {
+                    this.dia = dia;
+                }
+                else
+                {
+                    Console.WriteLine("Dia inválido");
+                    this.dia = 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Mês inválido");
+                this.mes = 1;
+            }
+
+            if (ano > 0)
+            {
+                this.ano = ano;
+            }
+            else
+            {
+                Console.WriteLine("Ano inválido");
+                this.ano = 1900;
+            }
+        }
+
+        public string getData()
+        {
+            return this.dia + "/" + this.mes + "/" + this.ano;
+        }
+    }
+
+    class Pessoa
+    {
+        public string nome;
+        public string cpf;
+        public string sexo;
+        public Data dataNasc;
+
+        public Pessoa(string nome, string cpf, string sexo)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.sexo = sexo;
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public string getCpf()
+        {
+            return this.cpf;
+        }
+
+        public string getSexo()
+        {
+            return this.sexo;
+        }
+        
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void setCpf(string cpf)
+        {
+            this.cpf = cpf;
+        }
+
+        public void setSexo(string sexo)
+        {
+            this.sexo = cpf;
+        }
+
+        public void apresentarPessoa()
+        {
+            Console.WriteLine("nome:" + this.nome);
+            Console.WriteLine("cpf:" + this.cpf);
+            Console.WriteLine("sexo:" + this.sexo);
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data dataNascimento = new Data(29,07,1999);
+            //Console.WriteLine("Data de Nascimento:" + dataNascimento.dia + "/" + dataNascimento.mes + "/" + dataNascimento.ano);
+            //dataNascimento.dia = 40;
+            dataNascimento.setData(29, 07, 1999);
+            Console.WriteLine("Data de Nascimento:" + dataNascimento.getData());
+
+            Pessoa asp = new Pessoa("Alexios", "0123048204", "Masculino");
+            asp.apresentarPessoa();
+
+            Console.ReadKey();
+        }
+    }
+}
